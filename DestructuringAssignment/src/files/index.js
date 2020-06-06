795b5558a7dadccc3e4105f5b831f572fca0cec0
DR-Edward <reyesiovanny@gmail.com>
2020-06-06 00:14:34 -0500

    way 7

--- DestructuringAssignment/src/files/index.js
+++ DestructuringAssignment/src/files/index.js
@@ -6,12 +6,15 @@ const data = {
     father: "Harry Smith",
     sister: "Samantha Smith"
   },
+  languajes: [ 'JavaScript', 'PHP', 'Java' ],
 };
 
 const printer = (data) => {
-  const { name, age: yearsOld, family: { mother : mom }, country = 'México' } = data;
+  const { name, age: yearsOld, family: { mother : mom }, country = 'México', languajes } = data;
 
-  console.log(`My name is ${ name } and I'm ${ yearsOld } years old. My mother's name is ${ mom } and we live in ${ country }`);
+  const mostLiked = languajes[0];
+
+  console.log(`My name is ${ name } and I'm ${ yearsOld } years old. My mother's name is ${ mom } and we live in ${ country }. My favorite programing languaje is ${ mostLiked }`);
 };
 
 printer(data);
